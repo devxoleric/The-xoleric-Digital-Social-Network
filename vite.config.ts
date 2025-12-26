601dd524b3c151589dbab7668c7849ffeac40712
devxoleric <devxoleric@gmail.com>
2025-12-27 02:50:56 +0300

    Add Vite configuration for React project

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  base: '/The-xoleric-Digital-Social-Network/',
+  plugins: [react()],
+});
